a380dd7cc0e6f9bfaa1e3285de3380cfb82bd9d7
guogeer <1500065870@qq.com>
2018-06-04 16:17:09 +0800

    test "go get" remote package

--- main/router_server.go
+++ main/router_server.go
@@ -3,14 +3,14 @@ package main
 import (
 	"fmt"
 	// "game"
-	"net"
-	_ "router"
-	"runtime"
-	"strconv"
 	"github.com/guogeer/husky/cmd"
 	"github.com/guogeer/husky/env"
 	"github.com/guogeer/husky/log"
+	_ "github.com/guogeer/husky/router"
 	"github.com/guogeer/husky/util"
+	"net"
+	"runtime"
+	"strconv"
 )
 
 func main() {
