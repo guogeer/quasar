c6faef74ba679a936c2ee53c7143cc5e5f713296
ko <kojavaee@aliyun.com>
2021-11-19 15:51:39 +0800

    添加indexFunc

--- randutil/rand.go
+++ randutil/rand.go
@@ -36,17 +36,18 @@ func ShuffleN(a interface{}, n int) {
 }
 
 // 根据数组权重随机多个不重复的结果，返回数组下标
-func IndexN(a interface{}, num int) []int {
-	var numbers []int64
+// 传入randBase的函数，是为了获取多个数组里面的基础概率
+func IndexFunc(a interface{},num int,randBase func(i int) int) []int{
 	var vals = reflect.ValueOf(a)
 
 	if num == -1 {
 		num = vals.Len()
 	}
+
+	// 计算总的概率数：多个数组里面通过randBase返回该数组的基础概率
+	var numbers []int64
 	for i := 0; i < vals.Len(); i++ {
-		s := fmt.Sprintf("%v", vals.Index(i))
-		f, _ := strconv.ParseFloat(s, 64)
-		numbers = append(numbers, int64(f*sampleSize))
+		numbers = append(numbers, int64(randBase(i)))
 	}
 
 	var res []int
@@ -69,6 +70,44 @@ func IndexN(a interface{}, num int) []int {
 		}
 	}
 	return res
+}
+
+// 根据数组权重随机多个不重复的结果，返回数组下标
+func IndexN(a interface{}, num int) []int {
+	 values := reflect.ValueOf(a)
+
+	return IndexFunc(a,num, func(i int) int {
+		s := fmt.Sprintf("%v", values.Index(i))
+		f, _ := strconv.ParseFloat(s, 64)
+		return int(f*sampleSize)
+	})
+	//
+	//for i := 0; i < vals.Len(); i++ {
+	//	s := fmt.Sprintf("%v", vals.Index(i))
+	//	f, _ := strconv.ParseFloat(s, 64)
+	//	numbers = append(numbers, int64(f*sampleSize))
+	//}
+	//
+	//var res []int
+	//for try := 0; try < num; try++ {
+	//	var part, sum int64
+	//	for _, n := range numbers {
+	//		sum += n
+	//	}
+	//
+	//	if sum > 0 {
+	//		r := rand.Int63n(sum)
+	//		for i, n := range numbers {
+	//			part += n
+	//			if r < part {
+	//				res = append(res, i)
+	//				numbers[i] = 0
+	//				break
+	//			}
+	//		}
+	//	}
+	//}
+	//return res
 }
 
 // 根据a[i]比重随机下标i
