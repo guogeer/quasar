3beaa4c03a2a961ab26d0eff197fdbce1cfa6569
Guogeer <1500065870@qq.com>
2021-12-15 11:06:10 +0800

    cmd.M：转JSON时忽略nil字段

---
+++ internal/cmd_test.go
@@ -0,0 +1,33 @@
+package internal
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/guogeer/quasar/util"
+)
+
+type mStruct struct {
+	N int
+}
+
+func TestM(t *testing.T) {
+	m1 := M{
+		"Slice":     []int{1, 2},
+		"NilSlice":  ([]int)(nil),
+		"Struct":    mStruct{N: 1},
+		"NilStruct": (*mStruct)(nil),
+		"Nil":       nil,
+		"String":    "StringA",
+	}
+	m2 := M{
+		"Slice":  []int{1, 2},
+		"Struct": mStruct{N: 1},
+		"String": "StringA",
+	}
+	if !util.EqualJSON(m1, m2) {
+		buf1, _ := json.Marshal(m1)
+		buf2, _ := json.Marshal(m2)
+		t.Errorf("check M.MarshalJSON m1:%s m2:%s", buf1, buf2)
+	}
+}
